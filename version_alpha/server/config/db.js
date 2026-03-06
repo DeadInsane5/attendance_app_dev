7d27abdfbc5fbe297f60296da49715f58addac98
DeadInsane5 <aditya.ajaykumar.2006@gmail.com>
2026-03-06 22:46:26 +0530

    added db config

---
+++ version_alpha/server/config/db.js
@@ -0,0 +1,15 @@
+import mysql from "mysql2/promise"
+import "dotenv/config.js"
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+})
+
+export default db
